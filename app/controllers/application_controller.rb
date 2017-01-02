d5ca333844bb33c6c00726f499dcc2bdd3befb64
Ian Gustafson <iangustafson@gmail.com>
2017-01-02 04:45:30 -0500

    Subheadings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 #  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :select_subheading
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(display_name))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(display_name))
+  end
+
+  def select_subheading
+    @subheading = Rails.configuration.subheadings['subheadings'].sample
   end
 
   before_action :store_current_location, unless: :devise_controller?
---
+++ config/initializers/subheadings.rb
@@ -0,0 +1,4 @@
+# Load subheadings
+
+Rails.application.config.subheadings = Rails.application.config_for(:subheadings)
+Rails.application.config.funtimes = {foo: 'bar'}
